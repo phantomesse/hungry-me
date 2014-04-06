e3ff39b6136ff70d29f4e64a79ff00df8fa3b182
Aftab Khan <aftabjkhan@gmail.com>
2014-04-05 20:05:31 -0400

    Added subclass for Delivery.com API communication

--- src/hungryme/DeliveryRequestServlet.java
+++ src/hungryme/DeliveryRequestServlet.java
@@ -9,8 +9,26 @@ public class DeliveryRequestServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		
+		RequestParams request = Common.processRequest(req);
+		if (request.getCategory() == null){
+			// if category is null, request determines available categories
+			
+			
+		}
+		
+		
 		resp.setContentType("text/plain");
 		//resp.getWriter().println("Delivery");
+		
+		
+	}
+	
+	static class DeliveryDotComApi{
+		private static String API_KEY = "MGY1MTQzOWIwZTBkMmUyNjM2NjM2NTM4MjNkNGJjNjE5";
+		
+		public static String queryCategory(){
+			return null;
+		}
 		
 	}
 	
