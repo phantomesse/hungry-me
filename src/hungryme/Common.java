34a2128d231f89d4603884430a90dfb502802e07
Aftab Khan <aftabjkhan@gmail.com>
2014-04-05 21:29:02 -0400

    Added backend url request methods, JSON reply parsing

--- src/hungryme/Common.java
+++ src/hungryme/Common.java
@@ -1,21 +1,27 @@
 package hungryme;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 import javax.servlet.http.HttpServletRequest;
 
 public class Common {
-	
+
 	public enum Feeling {
 		HUNGRY, THIRSTY;
 	}
-	
+
 	public final static String FEELING_PARAM_NAME = "feeling";
 	public final static String CATEGORY_PARAM_NAME = "category";
 	public final static String LATITUDE_PARAM_NAME = "lat";
 	public final static String LONGITUDE_PARAM_NAME = "long";
 	public final static String EXCLUDED_PARAM_NAME = "not";
-	
+
 	public final static String LIST_DELIMETER = "`";
-	
+
 	public static RequestParams processRequest(HttpServletRequest request) {
 
 		// Get feeling
@@ -25,10 +31,10 @@ public class Common {
 		} catch (Exception e) {
 			//TODO: There is an error
 		}
-		
+
 		// Get category (null if not present)
 		String category = (String) request.getParameter(CATEGORY_PARAM_NAME);
-		
+
 		// Get location
 		double latitude = 0, longitude = 0;
 		try {
@@ -38,13 +44,31 @@ public class Common {
 			//TODO: Error
 			e.printStackTrace();
 		}
-		
+
 		// Get excluded
 		String[] exclude = request.getParameter(EXCLUDED_PARAM_NAME).split(LIST_DELIMETER);
-		
+
 		// Create RequestParams
 		return new RequestParams(feeling, category, latitude, longitude, exclude);
-		
+
+	}
+
+	public static String requestHttp(URL url){
+		try{
+			URLConnection conn = url.openConnection();
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(conn.getInputStream()));
+			String inputLine;
+			StringBuilder reply = new StringBuilder();
+
+			while ((inputLine = in.readLine()) != null) 
+				reply.append(inputLine);
+			in.close();
+			return reply.toString();
+		} catch (IOException e){
+			//TODO: communication error
+			return null;
+		}
 	}
 
 }
--- src/hungryme/DeliveryRequestServlet.java
+++ src/hungryme/DeliveryRequestServlet.java
@@ -1,7 +1,20 @@
 package hungryme;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 
 @SuppressWarnings("serial")
 public class DeliveryRequestServlet extends HttpServlet {
@@ -12,7 +25,9 @@ public class DeliveryRequestServlet extends HttpServlet {
 		RequestParams request = Common.processRequest(req);
 		if (request.getCategory() == null){
 			// if category is null, request determines available categories
+			DeliveryDotComApi.queryNearbyCategories(request.getLatitude(), request.getLongitude());
 			
+			//TODO: RETURN RESULTS HERE
 			
 		}
 		
@@ -24,10 +39,55 @@ public class DeliveryRequestServlet extends HttpServlet {
 	}
 	
 	static class DeliveryDotComApi{
-		private static String API_KEY = "MGY1MTQzOWIwZTBkMmUyNjM2NjM2NTM4MjNkNGJjNjE5";
+		//private static String API_KEY = "MGY1MTQzOWIwZTBkMmUyNjM2NjM2NTM4MjNkNGJjNjE5";
+		private static final String API_BASE_URL = 
+				"https://api.delivery.com/merchant/search/delivery?client_id=MGY1MTQzOWIwZTBkMmUyNjM2NjM2NTM4MjNkNGJjNjE5";
+		private static final String JSON_MERCHANT_ARRAY_NAME = "merchants";
+		private static final String JSON_MERCHANT_SUMMARY_NAME = "summary";
+		private static final String JSON_MERCHANT_SUMMARY_CUISINES_ARRAY_NAME = "cuisines";
 		
-		public static String queryCategory(){
-			return null;
+		
+		public static String[] queryNearbyCategories(double latitude, double longitude){
+			ArrayList<String> nearbyCategories = new ArrayList<String>();
+			
+			try {
+				URL queryURL = new URL(API_BASE_URL + "&" + Double.toString(latitude) + "&" + Double.toString(longitude));
+				String ApiReplyJson = Common.requestHttp(queryURL);
+				
+				/*
+				 * {...,"merchants":
+				 * 		[
+				 * 			{...,"summary":
+				 * 				{...,"cuisines":
+				 * 					["Deli","Glatt Kosher","Kosher","American"]
+				 * 
+				 */
+				
+				JsonObject jobject = new JsonParser().parse(ApiReplyJson).getAsJsonObject();
+				JsonArray jarray = jobject.getAsJsonArray(JSON_MERCHANT_ARRAY_NAME);
+				Iterator<JsonElement> iter = jarray.iterator();
+				while (iter.hasNext()) {
+					jobject = iter.next().getAsJsonObject().getAsJsonObject(JSON_MERCHANT_SUMMARY_NAME);
+					JsonArray cuisines = jobject.getAsJsonArray(JSON_MERCHANT_SUMMARY_CUISINES_ARRAY_NAME);
+					Iterator<JsonElement> cuisinesIter = cuisines.iterator();
+					
+					String category;
+					while(cuisinesIter.hasNext()){
+						 category = iter.next().getAsJsonPrimitive().getAsString();
+						 if (!nearbyCategories.contains(category)){
+							 nearbyCategories.add(category);
+						 }
+					}
+					
+				}
+				
+				return (String[]) nearbyCategories.toArray();
+				
+			} catch (MalformedURLException e) {
+				// TODO: Malformed URL error
+				e.printStackTrace();
+				return null;
+			}
 		}
 		
 	}
