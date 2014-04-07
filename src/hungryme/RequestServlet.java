7cca237d22d6cdec7b9dfb103fa19d1000a3b8a6
Lauren Zou <zou.lauren@gmail.com>
2014-04-07 18:54:36 -0400

    More changes to request servlet abstract class.

--- src/hungryme/RequestServlet.java
+++ src/hungryme/RequestServlet.java
@@ -1,21 +1,30 @@
 package hungryme;
 
+import hungryme.data.Category;
 import hungryme.data.Feeling;
+import hungryme.data.Venue;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 public abstract class RequestServlet extends HttpServlet {
 	private enum ReturnType {
 		CATEGORIES, VENUES;
 	}
-	
+
 	private static final String PARAM_KEY_FEELING = "feeling";
 	private static final String PARAM_KEY_LATITUDE = "latitude";
 	private static final String PARAM_KEY_LONGITUDE = "longitude";
 	private static final String PARAM_KEY_CATEGORIES = "categories";
-	
+
 	protected Feeling feeling;
 	protected double latitude;
 	protected double longitude;
@@ -24,29 +33,52 @@ public abstract class RequestServlet extends HttpServlet {
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) {
 		// Parse request for parameters
+		parseHttpRequest(request);
 
+		switch (returnType) {
+		case CATEGORIES:
+			Category[] categoryResponse = getCategories();
+			break;
+		case VENUES:
+			Venue[] venueResponse = getVenues();
+			break;
+		}
 	}
+
+	protected abstract Category[] getCategories();
+
+	protected abstract Venue[] getVenues();
 
 	private void parseHttpRequest(HttpServletRequest request) {
 		// Get feeling
-		feeling = Feeling.valueOf(request
-				.getParameter(PARAM_KEY_FEELING));
+		feeling = Feeling.valueOf(request.getParameter(PARAM_KEY_FEELING));
 
 		// Get location (lat/lon)
 		try {
 			latitude = Double.valueOf(request.getParameter(PARAM_KEY_LATITUDE));
-			longitude = Double.valueOf(request.getParameter(PARAM_KEY_LONGITUDE));
+			longitude = Double.valueOf(request
+					.getParameter(PARAM_KEY_LONGITUDE));
 		} catch (NumberFormatException e) {
 			latitude = 0;
 			longitude = 0;
 		}
-		
+
 		// Get return type
 		returnType = ReturnType.valueOf(request.getParameter("returnType"));
-		
+
 		// Get categories if applicable
 		try {
-			request.getParameter(PARAM_KEY_CATEGORIES);
+			ArrayList<String> categoriesList = new ArrayList<String>();
+			JsonArray categoriesJson = new JsonParser().parse(
+					request.getParameter(PARAM_KEY_CATEGORIES))
+					.getAsJsonArray();
+			Iterator<JsonElement> iter = categoriesJson.iterator();
+			while (iter.hasNext()) {
+				String category = iter.next().getAsJsonPrimitive()
+						.getAsString();
+				categoriesList.add(category);
+			}
+			categories = categoriesList.toArray(new String[0]);
 		} catch (Exception e) {
 			categories = new String[0];
 		}
